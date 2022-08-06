9e2ea1cc5c2edb69daf2363959a8cdf21678debd
NgSonTung <ngsondecon@gmail.com>
2022-08-06 10:42:34 +0700

    Xong form login

---
+++ DoAnOOP/PControl/ControlAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnOOP.PControl
+{
+    internal class ControlAccount
+    {
+        public static doAnEntities db = ControlDataBase.qlhocvien;
+
+        public static List<Account> FindAll()
+        {
+            var aList = from s in db.Accounts select s;
+            return aList.ToList();
+        }
+
+        public static bool Login(string uName, string password)
+        {
+            bool result = false;
+            FindAll().ForEach(a =>
+            {
+                if (a.UserName == uName && a.Password == password)
+                    result = true;
+            });
+            return result;
+        }
+    }
+}
--- DoAnOOP/Program.cs
+++ DoAnOOP/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoAnOOP.PView;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace DoAnOOP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FManage());
+            Application.Run(new FLogin());
         }
     }
 }
